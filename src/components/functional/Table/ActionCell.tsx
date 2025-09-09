366e9a9ce0039217fa1ba91ae1d17ab9c61d0f0a
Viktor Stepanenco <vityamv@gmail.com>
2025-09-09 23:10:00 +0300

    add remove dialog

--- src/components/functional/Table/ActionCell.tsx
+++ src/components/functional/Table/ActionCell.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MoreHorizontal } from 'lucide-react';
 
@@ -9,6 +10,17 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import type { ISiteRecord } from '@/interfaces/site';
 import { useDeleteSite } from '@/store/siteSlice';
 import { Button } from '@/components/ui/button';
@@ -20,7 +32,6 @@ const ActionsCell = ({ site }: { site: ISiteRecord }) => {
 
   const handleDelete = async () => {
     try {
-      console.log('BOUT TO DELETE THIS ', site.id);
       await deleteSite(Number(site.id));
     } catch (err) {
       console.error('Failed to delete:', err);
@@ -28,42 +39,67 @@ const ActionsCell = ({ site }: { site: ISiteRecord }) => {
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className='h-8 w-8 p-0 cursor-pointer'>
-          <span className='sr-only'>Open menu</span>
-          <MoreHorizontal className='h-4 w-4' />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <DropdownMenuItem asChild>
-          <a
-            href={getLinkAddress(site.domainRegistrar)}
-            target='_blank'
-            rel='noopener noreferrer'
+    <div onClick={(e) => e.stopPropagation()}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='h-8 w-8 p-0 cursor-pointer'>
+            <span className='sr-only'>Open menu</span>
+            <MoreHorizontal className='h-4 w-4' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem asChild>
+            <a
+              href={getLinkAddress(site.domainRegistrar)}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Visit domain
+            </a>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <a
+              href={getLinkAddress(site.hostingProvider)}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Visit hosting
+            </a>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => navigate(`/app/edit-site/${site.id}`)}
           >
-            Visit domain
-          </a>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <a
-            href={getLinkAddress(site.hostingProvider)}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Visit hosting
-          </a>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => navigate(`/app/edit-site/${site.id}`)}>
-          Edit site
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={handleDelete} className='text-red-500'>
-          Remove site
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            Edit site
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem
+                onSelect={(e) => e.preventDefault()}
+                className='text-red-500'
+              >
+                Delete site
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  {`This action will remove record for site ${site.name}`}
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction type='button' onClick={handleDelete}>
+                  Delete
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 };
 
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 import Sidebar from '../components/functional/AppSidebar';
 import { SidebarProvider } from '../components/ui/sidebar';
 import Navbar from '@/components/functional/Navbar';
 
 const AppLayout = () => {
+  const [defaultOpen] = useState<boolean>(() => {
+    const v = Cookies.get('sidebar_state');
+    return v === undefined ? true : v === 'true';
+  });
+
   return (
-    <SidebarProvider defaultOpen={false}>
+    <SidebarProvider defaultOpen={defaultOpen}>
       <div className='flex min-h-screen bg-background w-full'>
         <Sidebar />
         <main className='w-full'>
